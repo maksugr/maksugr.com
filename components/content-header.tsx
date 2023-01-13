ce8d9169d64f41dcb9a2fb6a874292d132db62d9
Roman Ponomarev <maksugr@gmail.com>
2023-01-13 17:59:14 +0400

    Change font size of the article

--- components/content-header.tsx
+++ components/content-header.tsx
@@ -8,7 +8,7 @@ interface IContentHeaderProps {
 export const ContentHeader: FC<IContentHeaderProps> = ({ title, summary }) => {
     return (
         <section className='mb-12'>
-            <h1 className='text-5xl sm:text-6xl md:text-8xl'>{title}</h1>
+            <h1 className='text-5xl sm:text-6xl md:text-7xl'>{title}</h1>
             <span className='text-2xl block mt-4 text-gray-500'>{summary}</span>
         </section>
     );
